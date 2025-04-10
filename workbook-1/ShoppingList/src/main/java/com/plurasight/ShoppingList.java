5be8308964e2b646f65259a58d6cbc28b52af67b
Thebiginn3r <ant9462@gmail.com>
2025-04-10 09:28:12 -0400

    Initial commit

---
+++ workbook-1/ShoppingList/src/main/java/com/plurasight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.plurasight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("shopping list:");
+        System.out.println("oranges");
+        System.out.println("rice");
+        System.out.println("tv");
+        System.out.println("juice");
+        System.out.println("Light");
+        System.out.println("milk");
+        System.out.println("remote");
+        System.out.println("pears");
+        System.out.println("naruto");
+        System.out.println("flowers");
+
+    }
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/plurasight/helloworld.java
@@ -0,0 +1,8 @@
+package com.plurasight;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone I am Tony Brown");
+        System.out.println("raymond we need a break");
+    }
+}
