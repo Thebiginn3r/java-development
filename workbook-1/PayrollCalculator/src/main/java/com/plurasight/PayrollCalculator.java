a3bd2555e53e281fc6cf952705fb4ae4261faa47
Thebiginn3r <ant9462@gmail.com>
2025-04-11 18:02:42 -0400

    If Updated Payroll Calculator with overtime pay

--- workbook-1/PayrollCalculator/src/main/java/com/plurasight/PayrollCalculator.java
+++ workbook-1/PayrollCalculator/src/main/java/com/plurasight/PayrollCalculator.java
@@ -9,12 +9,20 @@ public class PayrollCalculator {
         String name = myScanner.nextLine();
 
         System.out.println("Enter Your hours worked: ");
-        float hoursWorked = myScanner.nextInt();
+        float hoursWorked = myScanner.nextFloat();
 
         System.out.println("Enter your hourly wage: ");
-        float wage = myScanner.nextInt();
+        float wage = myScanner.nextFloat();
+        double wage2 = wage * 1.5;
         float grosspay = hoursWorked * wage;
-        System.out.println(name + " worked " + hoursWorked + ". Their pay is " + grosspay);
+        double grosspay2 = (40 * wage) + ((hoursWorked - 40)* wage2);
+        if (hoursWorked > 40) {
+            System.out.println(name + " worked " + hoursWorked + ". Their pay is " + grosspay2);
+        } else {
+            System.out.println(name + " worked " + hoursWorked + ". Their pay is " + grosspay);
+        }
+
+
 
     }
 }
