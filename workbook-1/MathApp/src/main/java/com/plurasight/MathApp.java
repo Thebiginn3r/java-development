8a7be2ca1ba6430c265f931fb636ffe5c7f779b4
Thebiginn3r <ant9462@gmail.com>
2025-04-11 13:31:07 -0400

    Fixed completed Mod3 Exercise 2 - Mathapp

--- workbook-1/MathApp/src/main/java/com/plurasight/MathApp.java
+++ workbook-1/MathApp/src/main/java/com/plurasight/MathApp.java
@@ -3,13 +3,14 @@ package com.plurasight;
 public class MathApp {
     public static void main(String[] args) {
         // Question 1:
-// declare variables here
         double bobSalary = 105000;
         double garySalary = 120000;
         double highestSalary;
 
         double highestsalary = Math.max(bobSalary, garySalary);
         System.out.println("This the highest salary: " + highestsalary);
+
+        // Question 2:
 
         double carPrice = 12000;
         double truckPrice = 17225;
@@ -18,14 +19,18 @@ public class MathApp {
         cheaperChoice = Math.min(carPrice, truckPrice);
         System.out.println("The cheaper car: " + cheaperChoice);
 
+        // Question 3:
+
         double circrad = 7.25;
-        double area = Math.PI * circrad * circrad;
+        double area = Math.PI * Math.pow(circrad, 2);
         System.out.println("The area of the circle is " + area);
 
+        // Question 4:
         double tbd = 5;
         double sqRoot = Math.sqrt(tbd);
-        System.out.println("The square root is: " + sqRoot);
+        System.out.println("The square root of " + tbd + " is: " + sqRoot);
 
+        // Question 5:
         double x1 = 5;
         double y1 = 10;
         double x2 = 85;
@@ -33,12 +38,14 @@ public class MathApp {
         double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("The distance between the 2 points is " + distance);
 
+        // Question 6:
         double num = -3.8;
         double absolute = Math.abs(num);
         System.out.println("The absolute value is " + absolute);
 
+        // Question 7:
         System.out.println("Here is your random number: " + Math.random());
 
     }
-    
+
 }
