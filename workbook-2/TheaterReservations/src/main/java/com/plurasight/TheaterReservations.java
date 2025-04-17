268d0f80a73d80af8cdaa325a571096087635124
Thebiginn3r <ant9462@gmail.com>
2025-04-17 11:30:10 -0400

    Theater Reservations w/ Substring

--- workbook-2/TheaterReservations/src/main/java/com/plurasight/TheaterReservations.java
+++ workbook-2/TheaterReservations/src/main/java/com/plurasight/TheaterReservations.java
@@ -7,10 +7,16 @@ import java.util.Scanner;
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("First name: ");
+       /* System.out.print("First name: ");
         String firstName = scanner.nextLine().trim();
         System.out.print("Enter your last name: ");
-        String lastName = scanner.nextLine().trim();
+        String lastName = scanner.nextLine().trim();*/
+        System.out.println("Enter your first and last name:");
+        String fullName = scanner.nextLine();
+        int indexOfSpace = fullName.indexOf(" ");
+        String firstName = fullName.substring(0, indexOfSpace);
+        String lastName = fullName.substring(indexOfSpace + 1);
+
         System.out.println("What date will you be coming? (mm/dd/yyy):");
         String dateInput = scanner.nextLine();
         System.out.println("Enter how many tickets would you like:");
