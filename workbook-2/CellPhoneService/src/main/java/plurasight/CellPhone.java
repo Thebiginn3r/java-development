1597b8b713518ecb394be9c57601657e81de068f
Thebiginn3r <ant9462@gmail.com>
2025-04-18 14:16:33 -0400

    Cellphone Application

---
+++ workbook-2/CellPhoneService/src/main/java/plurasight/CellPhone.java
@@ -0,0 +1,68 @@
+package plurasight;
+
+public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "CellPhone{" +
+                "serialNumber=" + serialNumber +
+                ", model='" + model + '\'' +
+                ", carrier='" + carrier + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
+}
---
+++ workbook-2/CellPhoneService/src/main/java/plurasight/CellPhoneApplication.java
@@ -0,0 +1,45 @@
+package plurasight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone firstPhone = new CellPhone();
+
+        System.out.println("What is your serial number?");
+        int SerialNumber = scanner.nextInt();
+        scanner.nextLine();
+        firstPhone.setSerialNumber(SerialNumber);
+
+        System.out.println("What model is the phone?");
+        String model = scanner.nextLine();
+        firstPhone.setModel(model);
+
+        System.out.println("Who is the carrier?");
+        String carrier = scanner.nextLine();
+        firstPhone.setCarrier(carrier);
+
+        System.out.println("What is the phone number?");
+        String phoneNumber = scanner.nextLine();
+        firstPhone.setPhoneNumber(phoneNumber);
+
+        System.out.println("Who is the owner of the phone?");
+        String owner = scanner.nextLine();
+        firstPhone.setOwner(owner);
+
+        scanner.close();
+
+        System.out.println("\nCellphone Details:");
+        System.out.println("Serial Number: " + firstPhone.getSerialNumber());
+        System.out.println("Model: " + firstPhone.getModel());
+        System.out.println("Carrier: " + firstPhone.getCarrier());
+        System.out.println("Phone Number: " + firstPhone.getPhoneNumber());
+        System.out.println("Owner: " + firstPhone.getOwner());
+
+        System.out.println(firstPhone);
+
+
+    }
+}
